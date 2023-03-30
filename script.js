ed10e98849c63797e84f9bece3674e2dc195339a
CrissCross56 <grant.terdoslavich@gmail.com>
2023-03-30 16:27:31 -0400

    done

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 
-console.log(baseString);
+
 //have diff strings for the diff family members
-const rick = 'rick';
-const morty = 'morty';
-const summer = 'summer';
-const jerry = 'jerry';
-const beth = 'beth';
+//gain access to ur DOM elements
+let image = document.querySelector('#chara');
+let displayName = document.querySelector('#name');
+let displayInfo = document.querySelector('p');
+let prevButton = document.querySelector('#prev');
+let nextButton = document.querySelector('#next');
+let family;
+// let charaName = family[0];
+
+let iteration = 0;
 //have one here for mr poopy butthole
 
-                                    //                             V   put variables for name placeholders in here
-let currentURL = `https://rickandmortyapi.com/api/character/?name=${}`;
+                                    //                             
+let currentURL = `https://rickandmortyapi.com/api/character/?location=c137`;
 
 
-//have a function to determine what goes inside the template literal
-function userChoice(input){
+//attach event listeners
 
-    switch (input) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
-    }
 
-    return;
-}
-
-let firstSeason = [];
-//get the first 11 eps from the api
+//get the first five characters from earth c137, main cast family
+//make several requests
 fetch(currentURL)
   .then((res) => {
     return res.json();
@@ -36,8 +29,59 @@ fetch(currentURL)
   .then((response) => {
     //code goes here
     console.log("were getting here");
-    console.log(response);
+    // 0, 1, 2, 3, 4 '
+    family = []
+    for(let i = 0; i < 5; i ++){
+
+      family.push(response.results[i]);
+    }
+    
+  })
+  .then(() => {
+    prevButton.addEventListener('click', ()=> getPrev());
+    nextButton.addEventListener('click', () => getNext());
+    setUpPage();
   })
   .catch((err) => {
     //will throw an error for us if something goes wrong
   });
+
+function getPrev(){
+  iteration --;
+  if(iteration < 0){
+    iteration = family.length - 1;
+    setUpPage();
+  }
+  else{
+    console.log('we went down', iteration);
+    setUpPage();
+  }
+}
+
+function getNext(){
+  iteration++;
+  if(iteration > family.length - 1){
+    iteration = 0;
+    setUpPage();
+  }
+  else{
+    console.log('we plussed', iteration)
+    setUpPage();
+  }
+}
+
+function setUpPage(){
+  console.log(family[iteration]);
+  // console.log(family)
+  console.log(family[iteration].name)
+  displayName.innerText = family[iteration].name;
+  image.src = family[iteration].image;
+  displayInfo.innerText = `Status: ${family[iteration].status} 
+  Species: ${family[iteration].species}`
+  //set up event listeners
+  
+  
+}
+
+
+// setUpPage();
